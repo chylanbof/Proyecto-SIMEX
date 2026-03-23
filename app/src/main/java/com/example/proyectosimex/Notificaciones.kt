189e01f1c75b0c32ee3094f4a2674a0680129c35
ashy404 <thony1124@gmail.com>
2026-03-23 17:54:07 +0100

    popup notificaciones

--- app/src/main/java/com/example/proyectosimex/Notificaciones.kt
+++ app/src/main/java/com/example/proyectosimex/Notificaciones.kt
@@ -1,17 +1,26 @@
 package com.example.proyectosimex
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 
 class Notificaciones : AppCompatActivity(){
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_notificaciones)
-
+        setContentView(R.layout.popup_notificaciones)
         //Variable para llamar al boton externo
 
 
+
+
     }
+
 }
