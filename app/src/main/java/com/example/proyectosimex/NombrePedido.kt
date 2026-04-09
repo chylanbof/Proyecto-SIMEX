7012b2bd2f4e08492b1c3ea719cccc549a54e59d
Natalia Pérez Ruiz <mvillaltab2223@politecnics.barcelona>
2026-04-09 18:05:15 +0200

    empezado nombre pedido

---
+++ app/src/main/java/com/example/proyectosimex/NombrePedido.kt
@@ -0,0 +1,12 @@
+package com.example.proyectosimex
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class NombrePedido : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_nombre_pedido)
+}
